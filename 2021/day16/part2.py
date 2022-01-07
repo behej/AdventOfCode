84aa2b19f5d0e1519a59a4f3d9ea9eebb880537b
Jerome Behe <jerome.behe@fresenius-kabi.com>
2022-01-07 08:39:03 +0100

    day 16

---
+++ 2021/day16/part2.py
@@ -0,0 +1,151 @@
+
+class Packet:
+	def __init__(self, seq):
+		self.children = []
+		self.value = 0
+		self.version = bits_to_int(seq[:3])
+		del(seq[:3])
+		self.type = bits_to_int(seq[:3])
+		del(seq[:3])
+
+		# Packet is a value
+		if self.type == 4:
+			val_array = []
+			while seq[0]:
+				del(seq[0])
+				val_array += seq[:4]
+				del(seq[:4])
+			del(seq[0])
+			val_array += seq[:4]
+			del(seq[:4])
+			self.value = bits_to_int(val_array)
+		# Packet is an operation (contains subpackets)	
+		else:
+			self.length_type = seq[0]
+			del(seq[0])
+			# Length_type = True -> length = number of subpackets
+			if self.length_type:
+				self.length = bits_to_int(seq[:11])
+				del(seq[:11])
+			# Length type = False -> length = num of bits
+			else:
+				self.length = bits_to_int(seq[:15])
+				del(seq[:15])
+				initial_len = len(seq)
+			
+			while (self.length_type and len(self.children) < self.length) or (not self.length_type and initial_len-len(seq) < self.length):
+				self.children.append(Packet(seq))
+		
+	def sum_versions(self):
+		sum = self.version
+		for c in self.children:
+			sum += c.sum_versions()
+		return sum
+		
+		
+	def compute_value(self):
+		val = 0
+		
+		# Sum
+		if self.type == 0:
+			for c in self.children:
+				val += c.compute_value()
+		# Product
+		elif self.type == 1:
+			val = 1
+			for c in self.children:
+				val *= c.compute_value()
+		# Minimum
+		elif self.type == 2:
+			val = self.children[0].compute_value()
+			for c in self.children:
+				val = min(val, c.compute_value())
+		# Maximum
+		elif self.type == 3:
+			val = self.children[0].compute_value()
+			for c in self.children:
+				val = max(val, c.compute_value())
+		# Value
+		elif self.type == 4:
+			return self.value
+		# Greater than
+		elif self.type == 5:
+			return 1 if (self.children[0].compute_value() > self.children[1].compute_value()) else 0
+		# Less than
+		elif self.type == 6:
+			return 1 if (self.children[0].compute_value() < self.children[1].compute_value()) else 0
+		# Equal
+		elif self.type == 7:
+			val = 1 if (self.children[0].compute_value() == self.children[1].compute_value()) else 0
+		else:
+			print("Unknwown type")
+			
+		return val
+		
+		
+
+
+def bits_to_int(seq):
+	val = 0
+
+	for i in range(len(seq)):
+		if seq[-1-i]:
+			val += 2**i
+		
+	return val
+			
+
+
+def hex_to_bits(c):
+	list = []
+	if c == "0":
+		list = [False, False, False, False]
+	elif c == "1":
+		list = [False, False, False, True]
+	elif c == "2":
+		list = [False, False, True, False]
+	elif c == "3":
+		list = [False, False, True, True]
+	elif c == "4":
+		list = [False, True, False, False]
+	elif c == "5":
+		list = [False, True, False, True]
+	elif c == "6":
+		list = [False, True, True, False]
+	elif c == "7":
+		list = [False, True, True, True]
+	elif c == "8":
+		list = [True, False, False, False]
+	elif c == "9":
+		list = [True, False, False, True]
+	elif c == "A":
+		list = [True, False, True, False]
+	elif c == "B":
+		list = [True, False, True, True]
+	elif c == "C":
+		list = [True, True, False, False]
+	elif c == "D":
+		list = [True, True, False, True]
+	elif c == "E":
+		list = [True, True, True, False]
+	elif c == "F":
+		list = [True, True, True, True]	
+	
+	
+	return list
+
+
+line = ""
+
+# Load file
+with  open("input", "r") as f:
+	line = f.read().strip()
+	
+
+sequence = []
+for c in line:
+	sequence += hex_to_bits(c)
+	
+
+p = Packet(sequence)
+print("Computed value is: {}".format(p.compute_value()))
