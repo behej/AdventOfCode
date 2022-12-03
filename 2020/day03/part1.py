8b74cf4c8a4a65ce04ce5b231d56023ffa0d8d2d
Jérôme Béhé <jerome.behe@gmail.com>
2022-12-03 20:32:00 +0100

    Part 2

--- 2020/day03/part1.py
+++ 2020/day03/part1.py
@@ -1,19 +1,13 @@
 #! /usr/bin/python3
 
-import re
-
-
-coord = (0, 0)
 y = 0
 count = 0
-
 
 with open("input", "r") as f:
 	width = len(f.readline().strip())
 	for l in f:
 		y = (y + 3) % width
 		if l[y] == '#':
-			count += 1
-
+			count += 1 
 
 print(count)
