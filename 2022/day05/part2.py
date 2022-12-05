b03216010638fc0e8fee5cd9865bc4de89b01ef2
Jérôme Béhé <jerome.behe@gmail.com>
2022-12-05 20:50:24 +0100

    Day 05

---
+++ 2022/day05/part2.py
@@ -0,0 +1,35 @@
+#! /bin/python3
+
+import re
+
+COLS = 9
+stacks = []
+for i in range(COLS):
+    stacks.append([])
+
+
+with  open("input", "r") as f:
+    for l in f:
+        if l.strip() == "":
+            break
+
+        for i in range(COLS):
+            crate = l[i*4+1]
+            if crate != " " and not crate.isdigit():
+                stacks[i].append(crate)
+
+    for s in stacks:
+        s.reverse()
+
+    for l in f:
+        res = re.match("^move (\d+) from (\d+) to (\d+)$", l.strip())
+        qty = int(res.group(1))
+        orig = int(res.group(2)) - 1
+        dest = int(res.group(3)) - 1
+
+
+        stacks[dest].extend(stacks[orig][-qty:])
+        del(stacks[orig][-qty:])
+        
+word = "".join([s[-1] for s in stacks])
+print(word)
