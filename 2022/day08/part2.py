8a5bd5137091affe9fb4aee761c221cba3deb9c9
Jérôme Béhé <jerome.behe@gmail.com>
2022-12-08 19:53:05 +0100

    Improve part 2

--- 2022/day08/part2.py
+++ 2022/day08/part2.py
@@ -1,39 +1,27 @@
 #! /usr/bin/python3
 
-
-def compute_scenic(h, n, s, e, w):
+def compute_scenic(height, *directions):
     score = 1
-    n.reverse()
-    w.reverse()
-
-    for direction in [n, s, e, w]:
-        try:
-            tree = next(x for x in direction if x >= h)
-            score *= (direction.index(tree) + 1)
-        except StopIteration:
-            score *= len(direction)
+    for direction in directions:
+        score *= next((index+1 for index, x in enumerate(direction) if x >= h), len(direction))
 
     return score
-
-
 
 
 with open("input", "r") as f:
     rows = [l.strip() for l in f]
 
-
 max_score = 0
+for i in range(0, len(rows)):
+    for j in range(0, len(rows[i])):
+        h = int(rows[i][j])
 
-for i in range(1, len(rows)-1):
-    for j in range(1, len(rows[i])-1):
-        height = int(rows[i][j])
-
-        west = [int(k) for k in rows[i][:j]]
+        west = list(reversed([int(k) for k in rows[i][:j]]))
         east = [int(k) for k in rows[i][j+1:]]
-        north = [int(rows[k][j]) for k in range(i)]
+        north = list(reversed([int(rows[k][j]) for k in range(i)]))
         south = [int(rows[k][j]) for k in range(i+1, len(rows))]
 
-        score = compute_scenic(height, north, south, east, west)
+        score = compute_scenic(h, north, south, east, west)
         max_score = max(max_score, score)
 
 
