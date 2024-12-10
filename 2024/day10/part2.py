8bf2ff25aacef429b00e1f04c6f2edd6c4f954bd
Jérôme Béhé <jerome.behe@gmail.com>
2024-12-10 20:54:00 +0100

    part 2

---
+++ 2024/day10/part2.py
@@ -0,0 +1,48 @@
+#! /usr/bin/python3
+
+
+map = []
+
+def in_map(c):
+    return (0 <= c[0] < len(map) and 0 <= c[1] < len(map[0]))
+
+
+def find_path(c, origin):
+    val = map[c[0]][c[1]]
+
+    if val == 0:
+        start_points[c] += 1
+        return
+    
+    for i,j in [(-1,0), (1,0), (0,1), (0,-1)]:
+        new_c = (c[0]+i,c[1]+j)
+        if in_map(new_c):
+            if map[new_c[0]][new_c[1]] == val - 1:
+                find_path(new_c, origin)
+
+
+
+with open("input", "r") as f:
+    for l in f:
+        map.append([int(i) for i in l.strip()])
+
+start_points = {}
+end_points = []
+
+for y in range(len(map)):
+    for x in range(len(map[0])):
+        if map[y][x] == 0:
+            start_points[(y,x)] = 0
+        if map[y][x] == 9:
+            end_points.append((y,x))
+
+
+
+
+
+for p in end_points:
+    find_path(p, p)
+
+
+scores = sum(start_points.values())
+print(scores)
